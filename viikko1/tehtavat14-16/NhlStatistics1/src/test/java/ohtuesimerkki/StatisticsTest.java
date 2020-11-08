9496f1b5c67422f6d99d2ac04043e1ceeabcfa7d
Aleksi <paavola.aleksi@gmail.com>
2020-11-08 19:53:52 +0200

    finish week 1

---
+++ viikko1/tehtavat14-16/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,51 @@
+package ohtuesimerkki;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+
+    Reader readerStub = new Reader() {
+
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }
+
+    @Test
+    public void testikakkonen() {
+        assertEquals(new Player("Semenko", "EDM", 4, 12), readerStub.getPlayers().get(0));
+    }
+}
